7eec053950dda439e415bc4f52efa5d43717316b
tanverified <khantanveer2u@gmail.com>
2021-08-06 19:45:06 +0530

    City Tour Project Start

--- city_tour/src/App.js
+++ city_tour/src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-import Person from "./Person";
 
 function App() {
-  return <div className="person_list">
-      <Person id="61" name="Kim Harris" job="CEO" gender="men"/>
-      <Person id="76" name="John Doe" job="Full Stack Developer" gender="men"/>
-      <Person id="60" name="Nina Romero" job="UI Designer" gender="women"/>
-      <Person id="44" name="Enrique Anderson " job="Accountant" gender="men"/>
-      <Person id="51" name="Anna Hernandez" job="Human Resource Manager" gender="women"/>
-      <Person id="67" name="Wesley Dunn" job="Sales Manager" gender="men"/>
-  </div>;
+  return(
+    <>
+      <h1>App</h1>
+    </>
+  );
 }
 
 export default App;
